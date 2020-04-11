c6d7e1babbfa4bcfdae4ed04f4609ad8a3c84e73
onozaty <onozaty@gmail.com>
2020-04-11 23:49:16 +0900

    Spring Bootの雛形作成

---
+++ src/main/java/com/github/onozaty/attendance/AttendanceRecordChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.github.onozaty.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceRecordChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendanceRecordChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/onozaty/attendance/AttendanceRecordChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onozaty.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceRecordChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
