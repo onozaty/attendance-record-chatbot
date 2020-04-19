1f83bc6bc0e7ae0c331a4333508b15ef3b7f258d
onozaty <onozaty@gmail.com>
2020-04-20 00:10:57 +0900

    起動クラス名変更、dev-tools設定

--- src/main/java/com/github/onozaty/attendance/AttendanceRecorderApplication.java
+++ src/main/java/com/github/onozaty/attendance/AttendanceRecorderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AttendanceRecordChatbotApplication {
+public class AttendanceRecorderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AttendanceRecordChatbotApplication.class, args);
+        SpringApplication.run(AttendanceRecorderApplication.class, args);
     }
 
 }
