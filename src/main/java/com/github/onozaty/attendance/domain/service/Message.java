7bc4a8c9ebdfce1add502f36510ea1d5dbb8e77a
onozaty <onozaty@gmail.com>
2020-04-27 00:28:55 +0900

    JSONをデフォルトCamelCaseに変更(返却するデータはCamelCaseにしたかったので)

--- src/main/java/com/github/onozaty/attendance/domain/service/Message.java
+++ src/main/java/com/github/onozaty/attendance/domain/service/Message.java
@@ -2,6 +2,8 @@ package com.github.onozaty.attendance.domain.service;
 
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Message {
 
+    @JsonProperty("channel_name")
     private String channelName;
 
     private OffsetDateTime timestamp;
 
+    @JsonProperty("user_name")
     private String userName;
 
     private String text;
